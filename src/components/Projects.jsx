ee4d6111ab6dc91d6673445b237159265cc93a02
Amansingh1202 <aks001235@gmail.com>
2021-05-04 09:59:47 +0530

    Added projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,13 @@ import PartProject from "../components/PartProject";
 
 const ProjectList = [
   {
+    name: "Spam_Message_Detection",
+    description:
+      "This application detects whether the message entered by the user is spam or not using Naive Bayes Algorithm",
+    category: "Flask , Sklearn",
+    link: "https://github.com/Amansingh1202/Spam_Message_Detection",
+  },
+  {
     name: "Food-app",
     description: "A food application built using Nuxt.js",
     category: "Vue , Javascript",
@@ -25,6 +32,13 @@ const ProjectList = [
       "An application that provides information about marine animals",
     category: "Vue , Javascript , Golang",
     link: "https://github.com/Amansingh1202/marinelife-information-app",
+  },
+  {
+    name: "Calender-events",
+    description:
+      "This app is used to schedule events in the calender.The events are stored in the local storage of the browser.",
+    category: "Reactjs , Material UI",
+    link: "https://github.com/Amansingh1202/calender-events",
   },
 ];
 
